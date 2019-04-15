632a716adec3201023b1f875a8a5c4b96ec50d07
Travis Abeyti <tabeyti@gmail.com>
2019-04-15 01:20:47 -0700

    Conversion of "Execute" Pypline command finished.

---
+++ vscode/src/Logger.ts
@@ -0,0 +1,26 @@
+import * as vscode from 'vscode';
+
+/**
+ * TODO: Hack logger for output panel messages
+ */
+export class Logger {
+
+	private outputPanel: any;
+
+	constructor() {
+        this.outputPanel = vscode.window.createOutputChannel("Pypline Debug Output");
+        this.outputPanel.show();
+	}
+
+	public info(message: string) {
+		this.outputPanel.appendLine(`[I]: ${message}`);
+	}
+
+	public warn(message: string) {
+		this.outputPanel.appendLine(`[W]: ${message}`);
+	}
+
+	public error(message: string) {
+		this.outputPanel.appendLine(`[E]: ${message}`);
+	}
+}
--- vscode/src/extension.ts
+++ vscode/src/extension.ts
@@ -1,118 +1,78 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+// Extension Samples: https://github.com/Microsoft/vscode-extension-samples
+// Then (Promise) Usage: https://medium.com/patrickleenyc/things-to-keep-in-mind-while-writing-a-vs-code-extension-9f2a3369b799
+
 import * as vscode from 'vscode';
+import { Pypline } from './Pypline';
+import * as Config from './Config';
+import { Logger } from './Logger';
+var path = require('path');
 
-var _commands = [
+class PyplineCommand {
+    private context: vscode.ExtensionContext;
+    private pypline: Pypline;
+    [key:string]: any;
 
-		{
-			"caption": "Execute",
-			"command": "pypline",
-			"description": "Executes the current view as a pipeline job.",
-			"args": { "target": "pypline_execute" },
-			"children": []
-		},
-		{
-			"caption": "Abort",
-			"command": "pypline",
-			"description": "Aborts the active pipeline job initiated by Execute.",
-			"args": { "target": "pypline_abort" },
-			"children": []
-		},
-		{
-			"caption": "Update",
-			"command": "pypline",
-			"description": "Updates the current view's associated pipeline job configuration.",
-			"args": { "target": "pypline_update" },
-			"children": []
-		},
-		{
-			"caption": "Open Output Panel",
-			"command": "pypline",
-			"description": "Opens the active pipleine job's output panel.",
-			"args": { "target": "pypline_open_output_panel" },
-			"children": []
-		},
-		{
-			"caption": "Step Reference",
-			"command": "pypline",
-			"description": "Provides a list of pipelines steps from the targted Jenkins.",
-			"args": { "target": "pypline_step_reference" },
-			"children": []
-		},
-		{
-			"caption": "Global Variables Reference",
-			"command": "pypline",
-			"description": "Provides a list of steps from the Shares Library and global variables.",
-			"args": { "target": "pypline_global_vars_reference" },
-			"children": []
-		},
-		{
-			"caption": "Validate Declarative Pipeline",
-			"command": "pypline",
-			"description": "Validates the current view's declarative pipeline syntax.",
-			"args": { "target": "pypline_validate_dec_pipeline" },
-			"children": []
-		},    
-		{
-			"caption": "Jenkins",
-			"command": "pypline",
-			"description": "A list of Jenkins system commands.",
-			"args": { "target": "children" },
-			"children": [
-				{
-					"caption": "Run Console Groovy Script",
-					"command": "pypline",
-					"description": "Runs the current view's system groovy script (script console).",
-					"args": { "target": "jenkins_run_console_groovy_script" },
-					"children": []
-				},            
-				{
-					"caption": "Storage",
-					"command": "pypline",
-					"description": "Displays storage stats for all nodes.",
-					"args": { "target": "jenkins_node_storage" }
-				},
-				{
-					"caption": "Display",
-					"command": "pypline",
-					"description": "Allows a user to select a job to open on their browser.",
-					"args": { "target": "jenkins_job_display" }
-				},
-				{
-					"caption": "Download Build Log",
-					"command": "pypline",
-					"description": "Select a job and build to download the log.",
-					"args": { "target": "jenkins_job_download_build_log" },
-					"children": []
-				}            
-			]
-		}
-	];
+    constructor(context: vscode.ExtensionContext) {
+        this.context = context;
+        this.logger = new Logger();
+        this.pypline = new Pypline();
+		// this_window = window;
+    }
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+    private async evalOption(option: any) {
+        if (null !== option.children && option.children.length > 0) {
+            let result = await vscode.window.showQuickPick(option.children);
+            await this.evalOption(result);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "pypline" is now active!');
+            vscode.window.showQuickPick(option.children).then ((val: any) => {
+                this.evalOption(val);
+            });
+            return;
+        }
 
-	// var config = vscode.workspace.getConfiguration('pypline');
+        if (null === option.target) { return; }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.pypline', () => {
-		let quickpick: any[] = [];
-		_commands.forEach(item => {
-			quickpick.push({ label: item.caption, description: item.description })
-		});
+        // We have a command to execute. Use magic to do so.
+        await this[`${option.target}`]();
+    }
 
-		vscode.window.showQuickPick(quickpick);
-	});
+    private async pyplineExecuteCommand() {
+        // Validate it's valid groovy source.
+        var editor = vscode.window.activeTextEditor;
+        if (!editor) { return; }
+        if ("groovy" !== editor.document.languageId) {
+            return;
+        }
 
-	context.subscriptions.push(disposable);
+        // Grab filename to use as the Jenkins job name.
+        var jobname = path.parse(path.basename(editor.document.fileName)).name;
+
+        // Grab source from active editor.
+        let source = editor.document.getText();
+        if ("" === source) { return; }
+
+        await this.pypline.buildPipeline(source, jobname);
+    }
+
+    /**
+     * Displays the Pypline command list in quick pick.
+     */
+    public async displayCommands() {
+        let result = await vscode.window.showQuickPick(Config.getCommands());
+        if (undefined === result) { return; }
+        await this.evalOption(result);
+    }
 }
 
-// this method is called when your extension is deactivated
+export function activate(context: vscode.ExtensionContext) {
+
+	console.log('Extension Pypline now active!');
+
+    var pypline = new PyplineCommand(context);
+	let disposable = vscode.commands.registerCommand('extension.pypline', async () => {
+		await pypline.displayCommands();
+	});
+    context.subscriptions.push(disposable);
+}
+
 export function deactivate() {}
