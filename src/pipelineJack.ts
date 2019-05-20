dc6f7d6885635c7a6e02775f02e292d9936d6d48
Travis Abeyti <tabeyti@gmail.com>
2019-05-19 22:23:48 -0700

    update to pipelineJack to support build parameters
    a local params file will be created and used for pipeline builds if option is enabled

--- src/pipelineJack.ts
+++ src/pipelineJack.ts
@@ -2,59 +2,30 @@ import * as vscode from 'vscode';
 import * as xml2js from "xml2js";
 import * as util from 'util';
 import * as path from 'path';
+import * as fs from 'fs';
 
-import { getPipelineJobConfig } from './utils';
+import { getPipelineJobConfig, readjson, writejson } from './utils';
+import { sleep } from './utils';
 import { JenkinsService } from './jenkinsService';
 import { SharedLibApiManager, SharedLibVar } from './sharedLibApiManager';
 import { JackBase } from './jack';
 
 const parseXmlString = util.promisify(xml2js.parseString) as any as (xml: string) => any;
 
-/**
- * Struct for storing a Pipeline's build information.
- */
-class PipelineBuild {
-    job: string;
-    nextBuildNumber: number;
-    source: string;
-    hasParams: boolean;
-
-    constructor(
-        jobName: string,
-        source: string = '',
-        buildNumber: number = -1,
-        hasParams: boolean = false) {
-        this.job = jobName;
-        this.source = source;
-        this.nextBuildNumber = buildNumber;
-        this.hasParams = hasParams;
-    }
-}
-
 export class PipelineJack extends JackBase {
-    jobPrefix: string | undefined;
-    browserSharedLibraryRef: string;
-    browserBuildOutput: boolean;
-
-    timeoutSecs: number;
-    lastBuild?: PipelineBuild;
-    activeBuild?: PipelineBuild;
-    readonly sharedLib: SharedLibApiManager;
-    readonly pollMs: number;
-
-    readonly jenkins: JenkinsService;
+    private config: any;
+    private cachedJob?: any;
+    private activeJob?: any;
+    private readonly sharedLib: SharedLibApiManager;
+    private readonly jenkins: JenkinsService;
+    private readonly messageItem: vscode.MessageItem = {
+        title: 'Okay'
+    };
 
     constructor() {
         super('Pipeline Jack');
-        let pipelineConfig = vscode.workspace.getConfiguration('jenkins-jack.pipeline');
-        this.jobPrefix = pipelineConfig.jobPrefix;
-        this.browserBuildOutput = pipelineConfig.browserBuildOutput;
-        this.browserSharedLibraryRef = pipelineConfig.browserSharedLibraryRef;
+        this.config = vscode.workspace.getConfiguration('jenkins-jack.pipeline');
         vscode.workspace.onDidChangeConfiguration(event => { this.updateSettings(); });
-
-        this.timeoutSecs = 10;
-        this.pollMs = 100;
-
         this.jenkins = JenkinsService.instance();
         this.sharedLib = SharedLibApiManager.instance();
     }
@@ -63,7 +34,7 @@ export class PipelineJack extends JackBase {
         let commands: any[] = [];
 
         // Displayed commands altered by active pipeline build.
-        if (undefined === this.activeBuild) {
+        if (undefined === this.activeJob) {
             commands.push({
                 label: "$(triangle-right)  Pipeline: Execute",
                 description: "Executes the current groovy file as a pipeline job.",
@@ -95,18 +66,22 @@ export class PipelineJack extends JackBase {
     }
 
     public updateSettings() {
-        let pipelineConfig = vscode.workspace.getConfiguration('jenkins-jack.pipeline');
-        this.jobPrefix = pipelineConfig.jobPrefix;
-        this.browserBuildOutput = pipelineConfig.browserBuildOutput;
-        this.browserSharedLibraryRef = pipelineConfig.browserSharedLibraryRef;
+        this.config = vscode.workspace.getConfiguration('jenkins-jack.pipeline');
     }
 
     // @ts-ignore
     private async executePipeline() {
         // Validate it's valid groovy source.
         var editor = vscode.window.activeTextEditor;
-        if (!editor) { return; }
+        if (undefined === editor) { return; }
         if ("groovy" !== editor.document.languageId) {
+            return;
+        }
+
+        // Validate there is an associated file with the view/editor.
+        if ("untitled" === editor.document.uri.scheme) {
+            // TODO: prompt the save dialog for the Untitled file.
+            this.showInformationMessage('Must save the document before you run.', this.messageItem);
             return;
         }
 
@@ -117,16 +92,27 @@ export class PipelineJack extends JackBase {
         let source = editor.document.getText();
         if ("" === source) { return; }
 
-        await this.build(source, jobName);
+        // Build the pipeline.
+        this.activeJob = await this.build(source, jobName);
+        if (undefined === this.activeJob) { return; }
+
+        // Stream the output. Yep.
+        await this.jenkins.streamBuildOutput(
+            this.activeJob.fullName,
+            this.activeJob.nextBuildNumber,
+            this.outputChannel);
+
+        this.cachedJob = this.activeJob;
+        this.activeJob = undefined;
     }
 
     /**
      * Aborts the active pipeline build.
      */
     public async abortPipeline() {
-        if (undefined === this.activeBuild) { return; }
-        await this.jenkins.client.build.stop(this.activeBuild.job, this.activeBuild.nextBuildNumber).then(() => { });
-        this.activeBuild = undefined;
+        if (undefined === this.activeJob) { return; }
+        await this.jenkins.client.build.stop(this.activeJob.fullName, this.activeJob.nextBuildNumber).then(() => { });
+        this.activeJob = undefined;
     }
 
     // @ts-ignore
@@ -156,17 +142,17 @@ export class PipelineJack extends JackBase {
         let lib = await this.sharedLib.refresh() as SharedLibVar[];
         let result = await vscode.window.showQuickPick(lib);
         if (undefined === result) { return; }
-        if (this.browserSharedLibraryRef) {
-            if (undefined === this.lastBuild) {
+        if (this.config.browserSharedLibraryRef) {
+            if (undefined === this.cachedJob) {
                 this.jenkins.openBrowserAt(`pipeline-syntax/globals#${result.label}`);
             }
             else {
-                this.jenkins.openBrowserAt(`job/${this.lastBuild.job}/pipeline-syntax/globals#${result.label}`);
+                this.jenkins.openBrowserAt(`job/${this.cachedJob.fullName}/pipeline-syntax/globals#${result.label}`);
             }
         }
         else {
             const panel = vscode.window.createWebviewPanel(
-                'pipeline shared lib',
+                'Pipeline Shared Library',
                 result.label,
                 vscode.ViewColumn.Beside,
                 {}
@@ -178,37 +164,24 @@ export class PipelineJack extends JackBase {
     /**
      * Creates or update the provides job with the passed Pipeline source.
      * @param source The scripted Pipeline source.
-     * @param job The Jenkins Pipeline job name.
+     * @param jobName The Jenkins Pipeline job name.
+     * @returns A Jenkins 'job' json object.
      */
-    public async createUpdate(source: string, job: string) {
-        let jobName = job;
+    public async createUpdate(source: string, jobName: string): Promise<any> {
         let xml = getPipelineJobConfig();
-
-        // Format job name based on configuration setting.
-        if (undefined !== this.jobPrefix && this.jobPrefix.trim().length > 0) {
-            jobName = `${this.jobPrefix}-${jobName}`;
-        }
-
-        let build = new PipelineBuild(jobName, source);
-        let data = await this.jenkins.getJob(jobName);
+        let job = await this.jenkins.getJob(jobName);
 
         // If job already exists, grab the job config xml from Jenkins.
-        if (data) {
-            // Evaluated if this job has build parameters and set the next build number.
-            let param = data.property.find((p: any) => p._class.includes("ParametersDefinitionProperty"));
-            build.hasParams = param !== undefined;
-            build.nextBuildNumber = data.nextBuildNumber;
-
+        if (job) {
             // Grab job's xml configuration.
             xml = await this.jenkins.client.job.config(jobName).then((data: any) => {
                 return data;
             }).catch((err: any) => {
-                return undefined;
+                // TODO: Handle better
+                console.log(err);
+                throw err;
             });
         }
-
-        // TODO: should probably handle this somehow.
-        if (undefined === xml) { return; }
 
         // Inject the provided script/source into the job configuration.
         let parsed = await parseXmlString(xml);
@@ -217,21 +190,97 @@ export class PipelineJack extends JackBase {
         root.quietPeriod = 0;
         xml = new xml2js.Builder().buildObject(parsed);
 
-        if (!data) {
-            let userInput = await vscode.window.showInputBox({
-                placeHolder: "Job doesn't exist. Hit ENTER to create it or ESC to cancel."
-            });
-            if (undefined === userInput) { return; }
+        if (!job) {
+            let r = await this.showInformationMessage(
+                `"${jobName}" doesn't exist. Do you want us to create it?`, { modal: true }, "Yes");
+            if (undefined === r) { return undefined; }
 
             console.log(`${jobName} doesn't exist. Creating...`);
-            await this.jenkins.client.job.create(jobName, xml);
+            job = await this.jenkins.client.job.create(jobName, xml);
         }
         else {
             console.log(`${jobName} already exists. Updating...`);
             await this.jenkins.client.job.config(jobName, xml);
         }
         console.log(`Successfully updated Pipeline: ${jobName}`);
-        return build;
+        return job;
+    }
+
+    /**
+     * Handles the build parameter input flow for a Pipeline job
+     * and the creation/updating of the associated *.params.json
+     * file for the active editor's groovy file.
+     * @param job The jenkins Pipeline job json object.
+     * @returns A parameters key/value json object.
+     *          Undefined if job has no parameters.
+     *          An empty json if pamaeters are disabled.
+     */
+    public async buildParameterInput(
+        job: any,
+        progress: vscode.Progress<{ message?: string | undefined; increment?: number | undefined; }>): Promise<any> {
+
+        // Validate job has parameters.
+        let paramProperty = job.property.find((p: any) => p._class === "hudson.model.ParametersDefinitionProperty");
+        if (undefined === paramProperty) { return undefined; }
+
+        // Validate configuration enabled.
+        if (!this.config.params.enabled) { return {}; }
+
+        // Validate active editor.
+        if (undefined === vscode.window.activeTextEditor) {
+            throw new Error("No active editor to grab document path.");
+        }
+        let groovyScriptPath = vscode.window.activeTextEditor.document.uri.fsPath;
+
+        // Generate params path.
+        let parsed = path.parse(groovyScriptPath);
+        let paramsFileName = `${parsed.name}.params.json`;
+        let paramsPath = path.join(parsed.dir, paramsFileName);
+
+        // Gather parameter name/default-value json.
+        let infoMessage = `${paramsPath} created!`;
+        let paramsJson: any = {};
+        for (let p of paramProperty.parameterDefinitions) {
+            paramsJson[p.name] = p.defaultParameterValue.value;
+        }
+
+        let jsonParamsFileExist = fs.existsSync(paramsPath);
+
+        // If there are existing parameters for this job, update the job's
+        // defaults with the saved values.
+        if (jsonParamsFileExist) {
+            infoMessage = `${paramsPath} updated!`;
+
+            let json = readjson(paramsPath);
+            for (let key in json) { paramsJson[key] = json[key]; }
+        }
+
+        // Write the parameters file local to the editor's file.
+        writejson(paramsPath, paramsJson);
+        progress.report({ message: infoMessage });
+
+        // If there were no parameters file originally, present the defaults
+        // to the user in the editor to edit.
+        if (!jsonParamsFileExist) {
+            // Open the parameters file for the user to edit and wait
+            // on it's closing.
+            let openPath = vscode.Uri.parse(`file:///${paramsPath}`);
+            progress.report({ message: `${this.name}: Close ${paramsFileName} to continue build.` });
+            await vscode.window.showTextDocument(openPath, { viewColumn: vscode.ViewColumn.Beside });
+
+            // TODO: Don't know a better way on awaiting on a particular
+            // text editor closing.
+            let paramsEditorActive = undefined;
+            do {
+                await sleep(1000);
+                paramsEditorActive = vscode.window.visibleTextEditors.find(
+                    (e: vscode.TextEditor) => e.document.uri.path.includes(paramsFileName));
+            } while(undefined !== paramsEditorActive);
+
+        }
+
+        // Read saved/updated parameters and return to caller.
+        return readjson(paramsPath);
     }
 
     /**
@@ -240,8 +289,8 @@ export class PipelineJack extends JackBase {
      * @param job The name of the job to update.
      */
     public async update(source: string, job: string) {
-        if (undefined !== this.activeBuild) {
-            this.showWarningMessage(`Already building/streaming - ${this.activeBuild.job}: #${this.activeBuild.nextBuildNumber}`, undefined);
+        if (undefined !== this.activeJob) {
+            this.showWarningMessage(`Already building/streaming - ${this.activeJob.fullName}: #${this.activeJob.nextBuildNumber}`, undefined);
             return;
         }
 
@@ -268,14 +317,18 @@ export class PipelineJack extends JackBase {
      * Builds the targeted job with the provided Pipeline script/source.
      * @param source Scripted Pipeline source.
      * @param jobName The name of the job.
+     * @returns The Jenkins job json object of the build, where nextBuildNumber
+     *          represents the active build number.
+     *          Undefined if cancellation or failure to complete flow.
      */
     public async build(source: string, job: string) {
-        if (undefined !== this.activeBuild) {
-            this.showWarningMessage(`Already building/streaming - ${this.activeBuild.job}: #${this.activeBuild.nextBuildNumber}`, undefined);
-            return;
+
+        if (undefined !== this.activeJob) {
+            this.showWarningMessage(`Already building/streaming - ${this.activeJob.fullName}: #${this.activeJob.nextBuildNumber}`, undefined);
+            return undefined;
         }
 
-        vscode.window.withProgress({
+        return await vscode.window.withProgress({
             location: vscode.ProgressLocation.Notification,
             title: `Pipeline ${job}`,
             cancellable: true
@@ -285,36 +338,44 @@ export class PipelineJack extends JackBase {
             });
 
             progress.report({ increment: 0, message: `Creating/updating Pipeline job.` });
-            this.activeBuild = await this.createUpdate(source, job);
-            if (undefined === this.activeBuild) { return; }
+            let currentJob = await this.createUpdate(source, job);
+            if (undefined === currentJob) { return; }
 
-            // TODO: figure out a nice, user friendly, way that allows users to input build parameters
-            // for their pipeline builds. For now, we pass empty params to ensure it builds.
-            progress.report({ increment: 30, message: `Building "${this.activeBuild.job} #${this.activeBuild.nextBuildNumber}` });
-            let buildOptions = this.activeBuild.hasParams ? { name: this.activeBuild.job, parameters: {} } : { name: this.activeBuild.job };
+            let jobName = currentJob.fullName;
+            let buildNum = currentJob.nextBuildNumber;
+
+            if (token.isCancellationRequested) { return undefined;  }
+
+            // TODO: config conditional
+            progress.report({ increment: 20, message: `Waiting on build paramter input...` });
+            let params = {};
+            try {
+                params = await this.buildParameterInput(currentJob, progress);
+            } catch (err) {
+                this.showWarningMessage(err.message, undefined);
+                return undefined;
+            }
+
+            if (token.isCancellationRequested) { return undefined;  }
+
+            progress.report({ increment: 20, message: `Building "${jobName} #${buildNum}` });
+            let buildOptions = params !== undefined ? { name: jobName, parameters: params } : { name: jobName };
             await this.jenkins.client.job.build(buildOptions).catch((err: any) => {
                 console.log(err);
                 throw err;
             });
 
-            progress.report({ increment: 20, message: `Waiting for build to be ready...` });
-            await this.jenkins.buildReady(this.activeBuild.job, this.activeBuild.nextBuildNumber);
-            progress.report({ increment: 50, message: `Build is ready! Streaming output...` });
+            if (token.isCancellationRequested) { return undefined;  }
 
-            return new Promise(resolve => {
-                if (undefined === this.activeBuild) {
-                    resolve();
-                    return;
-                }
-                this.jenkins.streamOutput(
-                    this.activeBuild.job,
-                    this.activeBuild.nextBuildNumber,
-                    this.outputChannel, () => {
-                        this.lastBuild = this.activeBuild;
-                        this.activeBuild = undefined;
-                    });
-                resolve();
-            });
+            progress.report({ increment: 30, message: `Waiting for build to be ready...` });
+            try {
+                await this.jenkins.buildReady(jobName, buildNum);
+            } catch (err) {
+                this.showWarningMessage(`Timed out waiting for build: ${jobName} #${buildNum}`, undefined);
+                return undefined;
+            }
+            progress.report({ increment: 30, message: `Build is ready!` });
+            return currentJob;
         });
     }
 }
