e2edb929e8a3484b3d33664a61747ddd9d3202ed
Travis Abeyti <tabeyti@gmail.com>
2019-09-04 00:33:23 -0700

    refactor of setOnline/setOffline

--- src/nodeJack.ts
+++ src/nodeJack.ts
@@ -23,20 +23,30 @@ export class NodeJack extends JackBase {
         ];
     }
 
-    public async onNodes(callback: function): Promise<any> {
+    /**
+     * Handles an input flow for performing and action on targeted nodes.
+     * @param onNodeAction Async callback that runs an action on a node
+     * label and returns output. 
+     * @param filter Optional filter on a jenkins API node.
+     */
+    public async onNodes(
+        onNodeAction: (node: string) => Promise<string>,
+        filter: ((node: any) => boolean) | undefined = undefined): Promise<any> {
+            
         let nodes = await JenkinsHostManager.host().getNodes();
-        nodes = nodes.filter((n: any) => n.displayName !== 'master' && n.offline);
+        if (undefined !== filter) {
+            nodes = nodes.filter(filter);
+        }
 
         if (undefined === nodes) { return; }
         nodes.map((n: any) => {
             n.label = n.displayName;
-            n.description = "$(check)";
+            n.description = n.offline ? "$(alert)" : "$(check)";
             n.target = n
         });
 
         let selections = await vscode.window.showQuickPick(nodes, { canPickMany: true }) as any;
         if (undefined === selections) { return; }
-
 
         vscode.window.withProgress({
             location: vscode.ProgressLocation.Notification,
@@ -47,18 +57,18 @@ export class NodeJack extends JackBase {
                 this.showWarningMessage("User canceled node command.");
             });
 
-            // Builds a list of console script http requests across the list of targeted machines
+            // Builds a list of parallel actions across the list of targeted machines
             // and awaits across all.
             let tasks = [];
             progress.report({ increment: 50, message: "Executing on target machine(s)" });
             for (let m of selections) {
                 let promise = new Promise(async (resolve) => {
                     try {
-                        await JenkinsHostManager.host().client.node.enable(m.label);
+                        let output = await onNodeAction(m.label);
+                        return resolve({ node: m.label, output: output })
                     } catch (err) {
                         return resolve({ node: m.label, output: err })
                     }
-                    return resolve({ node: m.label, output: 'Marked Online' });
                 });
                 tasks.push(promise);
             }
@@ -80,119 +90,24 @@ export class NodeJack extends JackBase {
     }
 
     public async setOnline() {
-        let nodes = await JenkinsHostManager.host().getNodes();
-        nodes = nodes.filter((n: any) => n.displayName !== 'master' && n.offline);
 
-        if (undefined === nodes) { return; }
-        nodes.map((n: any) => {
-            n.label = n.displayName;
-            n.description = "$(check)";
-            n.target = n
-        });
-
-        let selections = await vscode.window.showQuickPick(nodes, { canPickMany: true }) as any;
-        if (undefined === selections) { return; }
-
-
-        vscode.window.withProgress({
-            location: vscode.ProgressLocation.Notification,
-            title: `Console Script(s)`,
-            cancellable: true
-        }, async (progress, token) => {
-            token.onCancellationRequested(() => {
-                this.showWarningMessage("User canceled node command.");
-            });
-
-            // Builds a list of console script http requests across the list of targeted machines
-            // and awaits across all.
-            let tasks = [];
-            progress.report({ increment: 50, message: "Executing on target machine(s)" });
-            for (let m of selections) {
-                let promise = new Promise(async (resolve) => {
-                    try {
-                        await JenkinsHostManager.host().client.node.enable(m.label);
-                    } catch (err) {
-                        return resolve({ node: m.label, output: err })
-                    }
-                    return resolve({ node: m.label, output: 'Marked Online' });
-                });
-                tasks.push(promise);
-            }
-            let results = await Promise.all(tasks);
-
-            // Iterate over the result list, printing the name of the
-            // machine and it's output.
-            this.outputChannel.clear();
-            this.outputChannel.show();
-            for (let r of results as any[]) {
-                this.outputChannel.appendLine(this.barrierLine);
-                this.outputChannel.appendLine(r.node);
-                this.outputChannel.appendLine('');
-                this.outputChannel.appendLine(r.output);
-                this.outputChannel.appendLine(this.barrierLine);
-            }
-            progress.report({ increment: 50, message: `Output retrieved. Displaying in OUTPUT channel...` });
-        });
+        await this.onNodes(async (label: string) => {
+            await JenkinsHostManager.host().client.node.enable(label);
+            return 'Node Online!'
+        }, (n: any) => n.displayName !== 'master' && n.offline);
     }
 
     /**
      * Sets selected nodes offline with an optional message.
      */
     public async setOffline() {
-        let nodes = await JenkinsHostManager.host().getNodes();
-        nodes = nodes.filter((n: any) => n.displayName !== 'master' && !n.offline);
-
-        if (undefined === nodes) { return; }
-        nodes.map((n: any) => {
-            n.label = n.displayName;
-            n.description = "$(alert)";
-            n.target = n
-        });
-
-        let selections = await vscode.window.showQuickPick(nodes, { canPickMany: true }) as any;
-        if (undefined === selections) { return; }
 
         let offlineMessage = await vscode.window.showInputBox({ prompt: 'Enter an offline message.' });
         if (undefined === offlineMessage) { return; }
 
-        vscode.window.withProgress({
-            location: vscode.ProgressLocation.Notification,
-            title: `Console Script(s)`,
-            cancellable: true
-        }, async (progress, token) => {
-            token.onCancellationRequested(() => {
-                this.showWarningMessage(`User canceled script console execute.`);
-            });
-
-            // Builds a list of console script http requests across the list of targeted machines
-            // and awaits across all.
-            let tasks = [];
-            progress.report({ increment: 50, message: "Executing on target machine(s)" });
-            for (let m of selections) {
-                let promise = new Promise(async (resolve) => {
-                    try {
-                        await JenkinsHostManager.host().client.node.disable(m.label, offlineMessage);
-                    } catch (err) {
-                        return resolve({ node: m.label, output: err })
-                    }
-                    return resolve({ node: m.label, output: 'Marked Offline' });
-                });
-                tasks.push(promise);
-            }
-            let results = await Promise.all(tasks);
-
-            // Iterate over the result list, printing the name of the
-            // machine and it's output.
-            this.outputChannel.clear();
-            this.outputChannel.show();
-            for (let r of results as any[]) {
-                this.outputChannel.appendLine(this.barrierLine);
-                this.outputChannel.appendLine(r.node);
-                this.outputChannel.appendLine('');
-                this.outputChannel.appendLine(r.output);
-                this.outputChannel.appendLine(this.barrierLine);
-            }
-            progress.report({ increment: 50, message: `Output retrieved. Displaying in OUTPUT channel...` });
-        });
+        await this.onNodes(async (label: string) => {
+            await JenkinsHostManager.host().client.node.disable(label, offlineMessage);
+            return 'Node Offline'
+        }, (n: any) => n.displayName !== 'master' && !n.offline);
     }
 }
