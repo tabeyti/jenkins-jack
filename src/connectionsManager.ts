5b2ca23e7366ba2666c3a865b487779087f47a05
tabeyti <tabeyti@gmail.com>
2020-09-06 23:31:44 -0700

    edit connection command

--- src/connectionsManager.ts
+++ src/connectionsManager.ts
@@ -16,6 +16,10 @@ export class ConnectionsManager implements QuickpickSet {
             await this.addConnection();
         }));
 
+        ext.context.subscriptions.push(vscode.commands.registerCommand('extension.jenkins-jack.connections.edit', async (item?: ConnectionsTreeItem) => {
+            await this.editConnection(item?.connection);
+        }));
+
         ext.context.subscriptions.push(vscode.commands.registerCommand('extension.jenkins-jack.connections.delete', async (item?: ConnectionsTreeItem) => {
             await this.deleteConnection(item?.connection);
         }));
@@ -31,17 +35,22 @@ export class ConnectionsManager implements QuickpickSet {
     public get commands(): any[] {
         return [
             {
-                label: "$(settings)  Host Selection",
-                description: "Select a jenkins host to connect to.",
+                label: "$(settings)  Connections: Select",
+                description: "Select a jenkins host connection to connect to.",
                 target: async () => vscode.commands.executeCommand('extension.jenkins-jack.connections.select')
             },
             {
-                label: "$(add)  Add Host Connection",
-                description: "Add a jenkins host via input prompts.",
+                label: "$(add)  Connections: Add",
+                description: "Add a jenkins host connection via input prompts.",
                 target: async () => vscode.commands.executeCommand('extension.jenkins-jack.connections.add')
             },
             {
-                label: "$(circle-slash)  Delete Host Connection",
+                label: "$(edit)  Connections: Edit",
+                description: "Edit a jenkins host's connection info.",
+                target: async () => vscode.commands.executeCommand('extension.jenkins-jack.connections.edit')
+            },
+            {
+                label: "$(circle-slash)  Connections: Delete",
                 description: "Delete a jenkins host connection from settings.",
                 target: async () => vscode.commands.executeCommand('extension.jenkins-jack.connections.delete')
             }
@@ -58,10 +67,7 @@ export class ConnectionsManager implements QuickpickSet {
         return result.target();
     }
 
-    /**
-     * Updates the settings for this service.
-     */
-    public updateSettings() {
+    private updateSettings() {
         let config = vscode.workspace.getConfiguration('jenkins-jack.jenkins');
         let conn: any;
         for (let c of config.connections) {
@@ -80,61 +86,99 @@ export class ConnectionsManager implements QuickpickSet {
         this._host = new JenkinsService(conn.name, conn.uri, conn.username, conn.password);
     }
 
+    public get activeConnection(): any {
+        let config = vscode.workspace.getConfiguration('jenkins-jack.jenkins');
+        for (let c of config.connections) {
+            if (c.active) {
+                return c;
+            }
+        }
+        return undefined;
+    }
+
     /**
      * Provides an input flow for adding in a host to the user's settings.
      */
     public async addConnection() {
         let config = vscode.workspace.getConfiguration('jenkins-jack.jenkins');
 
-        // Have user enter a unique name for the host. If host name already exists, try again.
-        let hostName: string | undefined = undefined;
-        while (true) {
-            hostName = await vscode.window.showInputBox({
-                prompt: 'Enter in a unique name for your jenkins connection (e.g. JenkyMcJunklets)'
-            });
-            if (undefined === hostName) { return undefined; }
+        let conn = await this.getConnectionInput();
+        if (undefined === conn) { return; }
 
-            if (!config.connections.some((c: any) => c.name === hostName)) {
-                break;
-            }
-            vscode.window.showWarningMessage(`There is already a connection named "${hostName}". Please choose another.`);
-        }
-
-        let hostUri = await vscode.window.showInputBox({
-            prompt: 'Enter in your host uri, including protocol (e.g. http://127.0.0.1:8080)',
-            value: 'http://127.0.0.1:8080'
-        });
-        if (undefined === hostUri) { return undefined; }
-
-        let username = await vscode.window.showInputBox({
-            prompt: 'Enter in a username for authentication'
-        });
-        if (undefined === username) { return undefined; }
-
-        let password = await vscode.window.showInputBox({
-            prompt: `Enter in the password of "${username}" for authentication`
-        });
-        if (undefined === password) { return undefined; }
-
-        this._host = new JenkinsService(hostName, hostUri, username, password);
+        this._host = new JenkinsService(conn.name, conn.uri, conn.username, conn.password);
 
         // Add the connection to the list and make it the active one
         config.connections.forEach((c: any) => c.active = false);
         config.connections.push({
-            "name": hostName,
-            "uri": hostUri,
-            "username": username,
-            "password": password,
+            "name": conn.name,
+            "uri": conn.uri,
+            "username": conn.username,
+            "password": conn.password,
             "active": true
         });
 
         vscode.workspace.getConfiguration().update('jenkins-jack.jenkins.connections', config.connections, vscode.ConfigurationTarget.Global);
-        vscode.window.showInformationMessage(`Jenkins Jack: Host updated to ${hostName}: ${hostUri}`);
+        vscode.window.showInformationMessage(`Jenkins Jack: Host updated to ${conn.name}: ${conn.uri}`);
 
         // Refresh the connection tree and it's dependent tree views
         vscode.commands.executeCommand('extension.jenkins-jack.tree.connections.refresh');
     }
 
+    /**
+     * Provides an input flow for a user to edit a host's connection info.
+     * @param conn Optional connection object edit.
+     */
+    public async editConnection(conn?: any) {
+        let config = vscode.workspace.getConfiguration('jenkins-jack.jenkins');
+
+        if (!conn) {
+            let hosts = [];
+            for (let c of config.connections) {
+                hosts.push({
+                    label: c.name,
+                    description: `${c.uri} (${c.username})`,
+                    target: c
+                });
+            }
+
+            // Select a connection to edit
+            let result = await vscode.window.showQuickPick(hosts);
+            if (undefined === result) { return; }
+            conn = result.target;
+        }
+
+        // Prompt user to edit the connection fields
+        let editedConnection = await this.getConnectionInput(conn);
+        if (undefined === editedConnection) { return; }
+
+        // If the name of a connection was changed, ensure we update
+        // references of pipeline tree items to use the new name
+        if (editedConnection.name !== conn.name) {
+            let pipelineConfig = await vscode.workspace.getConfiguration('jenkins-jack.pipeline.tree');
+            let pipelineTreeItems = [];
+            for (let c of pipelineConfig.items) {
+                if (conn.name === c.hostId) {
+                    c.hostId = editedConnection.name;
+                }
+                pipelineTreeItems.push(c);
+            }
+            await vscode.workspace.getConfiguration().update('jenkins-jack.pipeline.tree.items', pipelineTreeItems, vscode.ConfigurationTarget.Global);
+        }
+
+        // Update connection and the global config.
+        config.connections.forEach((c: any) => {
+            if (c.name === conn.name && undefined !== editedConnection) {
+                c.name = editedConnection.name;
+            }
+        });
+        await vscode.workspace.getConfiguration().update('jenkins-jack.jenkins.connections', config.connections, vscode.ConfigurationTarget.Global);
+        vscode.commands.executeCommand('extension.jenkins-jack.tree.connections.refresh');
+    }
+
+    /**
+     * User flow for deleting a Jenkins host connection.
+     * @param conn Optional connection object to delete.
+     */
     public async deleteConnection(conn?: any) {
         let config = vscode.workspace.getConfiguration('jenkins-jack.jenkins');
         if (!conn) {
@@ -157,7 +201,6 @@ export class ConnectionsManager implements QuickpickSet {
             return c.name !== conn.name;
         });
         await vscode.workspace.getConfiguration().update('jenkins-jack.jenkins.connections', modifiedConnections, vscode.ConfigurationTarget.Global);
-
         vscode.window.showInformationMessage(`Host "${conn.name} ${conn.uri}" removed`);
 
         // If this host was active, make the first host in the list active.
@@ -209,5 +252,76 @@ export class ConnectionsManager implements QuickpickSet {
 
         // Refresh the connection tree and it's dependent tree views
         vscode.commands.executeCommand('extension.jenkins-jack.tree.connections.refresh');
+    }
+
+    private async getConnectionInput(jenkinsConnection?: JenkinsConnection): Promise<JenkinsConnection | undefined> {
+        let config = vscode.workspace.getConfiguration('jenkins-jack.jenkins');
+
+        // Have user enter a unique name for the host. If host name already exists, try again.
+        let hostName: string | undefined = undefined;
+        while (true) {
+            hostName = await vscode.window.showInputBox({
+                ignoreFocusOut: true,
+                prompt: 'Enter in a unique name for your jenkins connection (e.g. JenkyMcJunklets)',
+                value: jenkinsConnection?.name
+            });
+            if (undefined === hostName) { return undefined; }
+
+            if (!config.connections.some((c: any) => c.name === hostName)) {
+                break;
+            }
+            vscode.window.showWarningMessage(`There is already a connection named "${hostName}". Please choose another.`);
+        }
+
+        let hostUri = await vscode.window.showInputBox({
+            ignoreFocusOut: true,
+            prompt: 'Enter in your host uri, including protocol (e.g. http://127.0.0.1:8080)',
+            value: (jenkinsConnection) ? jenkinsConnection.uri : 'http://127.0.0.1:8080'
+        });
+        if (undefined === hostUri) { return undefined; }
+
+        let username = await vscode.window.showInputBox({
+            ignoreFocusOut: true,
+            prompt: 'Enter in a username for authentication',
+            value: jenkinsConnection?.username
+        });
+        if (undefined === username) { return undefined; }
+
+        let password = await vscode.window.showInputBox({
+            ignoreFocusOut: true,
+            prompt: `Enter in the password of "${username}" for authentication`,
+            value: jenkinsConnection?.password
+        });
+        if (undefined === password) { return undefined; }
+
+        return new JenkinsConnection(hostName, hostUri, username, password, );
+    }
+}
+
+export class JenkinsConnection {
+    private _name: string;
+    private _uri: string;
+    private _username: string;
+    private _password: string;
+
+    public constructor(hostName: string, hostUri: string, username: string, password: string) {
+        this._name = hostName;
+        this._uri = hostUri;
+        this._username = username;
+        this._password = password;
+    }
+
+    public get name() { return this._name; }
+    public get uri() { return this._uri; }
+    public get username() { return this._username; }
+    public get password() { return this._password; }
+
+    public get json() {
+        return {
+            name: this._name,
+            uri: this._uri,
+            username: this._username,
+            password: this._password
+        };
     }
 }
